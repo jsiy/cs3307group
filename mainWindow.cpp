9e3f79f8fe8a555182943a7c988f13fddae32ce0
Joseph Christian Syjuco Siy <jsiy@uwo.ca>
2021-11-08 12:46:35 -0500

    currentSel
    added current selection functionality, next step is to have state buttons write to text file

--- mainWindow.cpp
+++ mainWindow.cpp
@@ -9,220 +9,265 @@
 #include <QStateMachine>
 #include <QEventTransition>
 #include <QPropertyAnimation>
+#include <QPalette>
+#include <string>
 
 using std::cout;
-
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
 {
-    //lable the qt box so that the user knows its a light controller 
-    QLabel *controllerLabel = new QLabel("Light Controller");
+    currentSel = {"", "", ""};
+    std::string s = "";
+    for (int i = 0; i < 3; i++)
+    {
+        s += currentSel[i];
+        if (i != 2)
+        {
+            s += ", ";
+        }
+    }
+    current = new QTextEdit(this);
+    current->setText(QString::fromStdString(s));
+    //lable the qt box so that the user knows its a light controller
+    controllerLabel = new QLabel("Light Controller", this);
     controllerLabel->setAlignment(Qt::AlignCenter);
 
-
-    //create the slider 
-    QSlider *brightness = new QSlider(Qt::Horizontal);
-    brightness->setRange(1,5);
-    QLabel *brightLabel = new QLabel("Brightness");
-    QHBoxLayout *bright = new QHBoxLayout;
+    //create the slider
+    brightness = new QSlider(Qt::Horizontal, this);
+    brightness->setRange(1, 10);
+    brightLabel = new QLabel("Brightness", this);
+    bright = new QHBoxLayout;
     bright->addWidget(brightLabel);
     bright->addWidget(brightness);
 
-
     //creating the buttons:
     //the following will be on the same widet board
-    QPushButton *breathB = new QPushButton("Breath Lights");
-    QPushButton *trafficB = new QPushButton("Traffic Route Lights");
-    QPushButton *flahsB = new QPushButton("Flashing Lights");
-    QPushButton *musictrafficB = new QPushButton("Music");
-    QPushButton *musicbeatB = new QPushButton("Music To The Beat");
+    breathB = new QPushButton("Breath Lights", this);
+    trafficB = new QPushButton("Traffic Route Lights", this);
+    flashB = new QPushButton("Flashing Lights", this);
+    musictrafficB = new QPushButton("Music", this);
+    musicbeatB = new QPushButton("Music To The Beat", this);
     //QPushButton *solidB = new QPushButton("Solid Colours");
 
-
-    QVBoxLayout *patternButtons = new QVBoxLayout;
+    patternButtons = new QVBoxLayout;
     patternButtons->addWidget(breathB);
     patternButtons->addWidget(trafficB);
-    patternButtons->addWidget(flahsB);
+    patternButtons->addWidget(flashB);
     patternButtons->addWidget(musictrafficB);
     patternButtons->addWidget(musicbeatB);
     //patternButtons->addWidget(solidB);
 
+    states = new QHBoxLayout;
+    s1 = new QPushButton("State 1", this);
+    s2 = new QPushButton("State 2", this);
+    s3 = new QPushButton("State 3", this);
+    states->addWidget(s1);
+    states->addWidget(s2);
+    states->addWidget(s3);
 
-
-    //creating state machine
-    QStateMachine *machine = new QStateMachine(this); 
-
-    QState *s1 = new QState();
-    QState *s2 = new QState(); 
-    QState *s3 = new QState(); 
-
-    QPropertyAnimation *animation;
-
-     QLabel *label = new QLabel("Current state");
-     QPushButton *stateButton = new QPushButton("Change State");
-
-    s1->assignProperty(label, "text", "Current State : 1");
-    s1->assignProperty(stateButton, "geometry", QRect(50,200,10,50));
-
-    s2->assignProperty(label, "text", "Current State : 2");
-    s2->assignProperty(stateButton, "geometry", QRect(50,200,10,50));
-
-    s3->assignProperty(label, "text", "Current State : 3");
-    s3->assignProperty(stateButton, "geometry", QRect(50,200,10,50));
-
-
-    //creating the animation 
-    animation = new QPropertyAnimation(stateButton, "geometry");
-    animation->setEasingCurve(QEasingCurve::OutBounce);
-
-
-    //adding events 
-    QEventTransition *t1 = new QEventTransition(stateButton, QEvent::MouseButtonPress);
-    t1->setTargetState(s2);
-    t1->addAnimation(animation);
-    s1->addTransition(t1);
-
-    QEventTransition *t2 = new QEventTransition(stateButton, QEvent::MouseButtonPress);
-    t2->setTargetState(s3);
-    t2->addAnimation(animation);
-    s2->addTransition(t1);
-
-    QEventTransition *t3 = new QEventTransition(stateButton, QEvent::MouseButtonPress);
-    t3->setTargetState(s1);
-    t3->addAnimation(animation);
-    s3->addTransition(t1);
-
-    machine->addState(s1);
-    machine->addState(s2);
-    machine->addState(s3);
-    machine->setInitialState(s1);
-    machine->start();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    //so how are we going to do this one? will we just have a section on our controller thats going to say pick a solid colour and then have a bunch of buttons for solid colours 
-    //maybe we could create a widget on the main widget that has a bunch of colour buttons 
-    //so instead of making one button called solidb like i have bellow: 
-     //QPushButton *solidB = new QPushButton("Solid Colour");
-    //we could do this 
-    QPushButton *red = new QPushButton("Red");
-    QPushButton *blue = new QPushButton("blue");
-    QPushButton *green = new QPushButton("Green");
-    QPushButton *purple = new QPushButton("Purple");
-   //then we create a lable for the colours 
-    QLabel *colourLabel = new QLabel("Solid Colours: ");
-    //for example 
-    //creating a horizontal widget to store all the possible colours 
+    //so how are we going to do this one? will we just have a section on our controller thats going to say pick a solid colour and then have a bunch of buttons for solid colours
+    //maybe we could create a widget on the main widget that has a bunch of colour buttons
+    //so instead of making one button called solidb like i have bellow:
+    //QPushButton *solidB = new QPushButton("Solid Colour");
+    //we could do this
+    red = new QPushButton("Red", this);
+    blue = new QPushButton("Blue", this);
+    green = new QPushButton("Green", this);
+    purple = new QPushButton("Purple", this);
+    //then we create a lable for the colours
+    colourLabel = new QLabel("Solid Colours: ", this);
+    //for example
+    //creating a horizontal widget to store all the possible colours
     //this actually might be better as a vertical layout idk tho
-    QHBoxLayout *colourButtons = new QHBoxLayout;
+    colourButtons = new QHBoxLayout;
     colourButtons->addWidget(colourLabel);
     colourButtons->addWidget(red);
     colourButtons->addWidget(blue);
     colourButtons->addWidget(green);
     colourButtons->addWidget(purple);
 
-    //creating the main widet box to add the above on the one widget as the controller 
-    QVBoxLayout *controller = new QVBoxLayout;
+    //creating the main widet box to add the above on the one widget as the controller
+    controller = new QVBoxLayout;
     controller->addWidget(controllerLabel);
+    controller->addWidget(current);
+    controller->addLayout(states);
     controller->addLayout(bright);
     controller->addLayout(patternButtons);
     controller->addLayout(colourButtons);
 
     //this is going to make the controller the main widget
     QWidget *theWholeController = new QWidget;
-    //set the controller to be the main 
-    theWholeController->setLayout(controller); 
-    //the central widget is going to be the widget in which contains the controller layout 
+    //set the controller to be the main
+    theWholeController->setLayout(controller);
+    //the central widget is going to be the widget in which contains the controller layout
     setCentralWidget(theWholeController);
- 
- 
-    //again, this could change depending on how we want to do the solid colour 
+
+    //again, this could change depending on how we want to do the solid colour
     connect(breathB, &QPushButton::released, this, &MainWindow::breathButton);
     connect(trafficB, &QPushButton::released, this, &MainWindow::trafficButton);
-    connect(flahsB, &QPushButton::released, this, &MainWindow::flashButton);
+    connect(flashB, &QPushButton::released, this, &MainWindow::flashButton);
     connect(musictrafficB, &QPushButton::released, this, &MainWindow::musicTrafficButton);
     connect(musicbeatB, &QPushButton::released, this, &MainWindow::musicBeatButton);
-    //we would have to add more for the solid colours. 
+
+    connect(red, &QPushButton::released, this, &MainWindow::redB);
+    connect(blue, &QPushButton::released, this, &MainWindow::blueB);
+    connect(green, &QPushButton::released, this, &MainWindow::greenB);
+    connect(purple, &QPushButton::released, this, &MainWindow::purpleB);
+    //we would have to add more for the solid colours.
 
     //connect(solidB, &QPushButton::released, this, &MainWindow::solidColourButton);
 
     connect(brightness, &QSlider::valueChanged, this, &MainWindow::adjustBrightness);
-
 }
 
 void MainWindow::breathButton()
 {
-    
+    QString temp = breathB->text();
+    std::string buttonText = temp.toStdString();
+    currentSel[2] = buttonText;
+    std::string s = "";
+    for (int i = 0; i < 3; i++)
+    {
+        s += currentSel[i];
+        s += ", ";
+    }
+    current->setText(QString::fromStdString(s));
 }
 
 void MainWindow::trafficButton()
 {
-    
+    QString temp = trafficB->text();
+    std::string buttonText = temp.toStdString();
+    currentSel[2] = buttonText;
+    std::string s = "";
+    for (int i = 0; i < 3; i++)
+    {
+        s += currentSel[i];
+        s += ", ";
+    }
+    current->setText(QString::fromStdString(s));
 }
 
 void MainWindow::flashButton()
 {
-    
+    QString temp = flashB->text();
+    std::string buttonText = temp.toStdString();
+    currentSel[2] = buttonText;
+    std::string s = "";
+    for (int i = 0; i < 3; i++)
+    {
+        s += currentSel[i];
+        s += ", ";
+    }
+    current->setText(QString::fromStdString(s));
 }
 
 void MainWindow::musicTrafficButton()
 {
-    
+    QString temp = musictrafficB->text();
+    std::string buttonText = temp.toStdString();
+    currentSel[2] = buttonText;
+    std::string s = "";
+    for (int i = 0; i < 3; i++)
+    {
+        s += currentSel[i];
+        s += ", ";
+    }
+    current->setText(QString::fromStdString(s));
 }
-
-void MainWindow::solidColourButton()
-{
-    //if we ended up making different buttons for each colour, we would get rid of this and add more buttons in the header file for the colours :) just depends on how we do stuff with the led class.
-}
-
 
 void MainWindow::musicBeatButton()
 {
-    
+    QString temp = musicbeatB->text();
+    std::string buttonText = temp.toStdString();
+    currentSel[2] = buttonText;
+    std::string s = "";
+    for (int i = 0; i < 3; i++)
+    {
+        s += currentSel[i];
+        s += ", ";
+    }
+    current->setText(QString::fromStdString(s));
 }
 
-void MainWindow::adjustBrightness(){
-
+void MainWindow::redB()
+{
+    QString temp = red->text();
+    std::string buttonText = temp.toStdString();
+    currentSel[1] = buttonText;
+    std::string s = "";
+    for (int i = 0; i < 3; i++)
+    {
+        s += currentSel[i];
+        s += ", ";
+    }
+    current->setText(QString::fromStdString(s));
 }
-
+void MainWindow::blueB()
+{
+    QString temp = blue->text();
+    std::string buttonText = temp.toStdString();
+    currentSel[1] = buttonText;
+    std::string s = "";
+    for (int i = 0; i < 3; i++)
+    {
+        s += currentSel[i];
+        s += ", ";
+    }
+    current->setText(QString::fromStdString(s));
+}
+void MainWindow::greenB()
+{
+    QString temp = green->text();
+    std::string buttonText = temp.toStdString();
+    currentSel[1] = buttonText;
+    std::string s = "";
+    for (int i = 0; i < 3; i++)
+    {
+        s += currentSel[i];
+        s += ", ";
+    }
+    current->setText(QString::fromStdString(s));
+}
+void MainWindow::purpleB()
+{
+    QString temp = purple->text();
+    std::string buttonText = temp.toStdString();
+    currentSel[1] = buttonText;
+    std::string s = "";
+    for (int i = 0; i < 3; i++)
+    {
+        s += currentSel[i];
+        s += ", ";
+    }
+    current->setText(QString::fromStdString(s));
+}
+void MainWindow::s1B()
+{
+}
+void MainWindow::s2B()
+{
+}
+void MainWindow::s3B()
+{
+}
+void MainWindow::adjustBrightness()
+{
+    int brightVal = brightness->value();
+    std::string s = std::to_string(brightVal);
+    currentSel[0] = s;
+    for (int i = 0; i < 3; i++)
+    {
+        s += currentSel[i];
+        s += ", ";
+    }
+    current->setText(QString::fromStdString(s));
+}
 
 /*
  *deconstructor used to clean up 
  */
-MainWindow::~MainWindow(){
-
-   
- 
+MainWindow::~MainWindow()
+{
 }
--- mainWindow.h
+++ mainWindow.h
@@ -6,20 +6,23 @@
 #include <QPushButton>
 #include <QVBoxLayout>
 #include <QGroupBox>
+#include <QLabel>
+#include <QSlider>
 #include <QHBoxLayout>
-
+#include <QTextEdit>
 
 using std::string;
 using std::vector;
 
-
-//taken from the push example 
-namespace Ui {
+//taken from the push example
+namespace Ui
+{
     class MainWindow;
 }
 
-//taken from the push example 
-class MainWindow : public QMainWindow {
+//taken from the push example
+class MainWindow : public QMainWindow
+{
     Q_OBJECT
 public:
     explicit MainWindow(QWidget *parent = nullptr);
@@ -27,34 +30,55 @@ public:
     //Deconstructor incase any pointers need to be deleted to clean up space in the heap.
     ~MainWindow();
 
-
-//taken from the push example 
+    //taken from the push example
 private slots:
     void breathButton();
     void trafficButton();
     void flashButton();
     void musicTrafficButton();
-    void solidColourButton();
     void musicBeatButton();
+    void redB();
+    void blueB();
+    void greenB();
+    void purpleB();
+    void s1B();
+    void s2B();
+    void s3B();
     void adjustBrightness();
 
-
 private:
-    
-    //to create widgets 
+    //to create widgets
     QWidget *widget;
 
-    //to create buttons, we will end up creating 6 of these in the cpp file. 
-    QPushButton *createButton;
-  
-    //main box for the controller - i made this a vertical one but we can change this if you guys want
-    QVBoxLayout *mainB;
+    //elements of gui
+    QTextEdit *current;
 
-    //we could have the colour bottons on this widet or something (we can use another widget style i just picked a random, thoughts?)
-    QHBoxLayout *colour;
+    QPushButton *breathB;
+    QPushButton *trafficB;
+    QPushButton *flashB;
+    QPushButton *musictrafficB;
+    QPushButton *musicbeatB;
+    QPushButton *red;
+    QPushButton *blue;
+    QPushButton *green;
+    QPushButton *purple;
+    QPushButton *s1;
+    QPushButton *s2;
+    QPushButton *s3;
 
-    //here we would also include information from the LED light file once we make it 
-    
+    QHBoxLayout *colourButtons;
+    QHBoxLayout *bright;
+    QHBoxLayout *states;
+    QVBoxLayout *controller;
+    QBoxLayout *patternButtons;
+
+    QLabel *colourLabel;
+    QLabel *brightLabel;
+    QLabel *controllerLabel;
+
+    QSlider *brightness;
+
+    std::array<std::string, 3> currentSel; //[0] contains brightness, [1] contains colour, [2] contains mode
 };
 
 #endif // MAINWINDOW_H
